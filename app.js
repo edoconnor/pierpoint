ec998d5973bd0c04f3a69a161804ab3dfb0148c4
Ed O'Connor <ed@oconnor.app>
2022-09-13 07:48:55 -0400

    images update

---
+++ app.js
@@ -0,0 +1,10 @@
+const textDisplay = document.getElementById('text');
+const phrases = ['Hello, my name is Eddie', 'I`m a web developer']
+let i=on
+let j=0
+
+function loop (){
+    if (i < phrases.length) {
+        console.log(phrases[i])
+    }
+}
