f188a63e61be352050ce20bafc36c63fef366aee
Ed O'Connor <ed@oconnor.app>
2022-10-03 10:24:05 -0400

    text update

--- app.js
+++ app.js
@@ -6,6 +6,6 @@
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 tl.to(".text", { y: "0%", duration: 1, stagger: 0.5 });
-tl.to(".intro", { y: "-100%", duration: .5 }, "+=1");
+tl.to(".intro", { y: "-100%", duration: 0.5 }, "+=1");
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 2 });
  
